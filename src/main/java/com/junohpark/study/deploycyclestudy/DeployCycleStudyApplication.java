9043a7c1cf9800d122fcec795e1a25d8bf572f38
junohpark221 <junohpark221@gmail.com>
2024-05-02 22:11:27 +0900

    environment: Spring Boot 환경 세팅

---
+++ src/main/java/com/junohpark/study/deploycyclestudy/DeployCycleStudyApplication.java
@@ -0,0 +1,13 @@
+package com.junohpark.study.deploycyclestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployCycleStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployCycleStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junohpark/study/deploycyclestudy/DeployCycleStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junohpark.study.deploycyclestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployCycleStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
